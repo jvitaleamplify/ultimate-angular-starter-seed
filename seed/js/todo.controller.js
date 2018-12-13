4e52a7c6777a7b4c14c217676c050c50adf0fe1b
Jake Vitale <jvitale@amplify.com>
2018-12-13 10:34:56 -0500

    Lab 5

--- seed/js/todo.controller.js
+++ seed/js/todo.controller.js
@@ -1,34 +1,59 @@
-function TodoController(){
-    this.newTodo = ""
+function TodoController(TodoService){
+    
+    var ctrl = this
 
-    this.list = [{
-        title:'First todo item!',
-        completed:false
-    },{
-        title:'Second todo item!',
-        completed:false
-    },{
-        title:'Third todo item!',
-        completed:false
-    }];
-
-    this.addTodo = function(){
-        this.list.unshift({
-            title: this.newTodo,
-            completed: false
-        });
-        this.newTodo = "";
-    };
-
-    this.removeTodo = function(item, index){
-        this.list.splice(index, 1);
-    }
-
-    this.getRemaining = function(){
-        return this.list.filter(function(item){
-            return !item.completed;
+    ctrl.list = [];
+    ctrl.newTodo = ""
+    
+    function getTodos(){
+        TodoService.retrieve().then(function (response){
+            ctrl.list = response
         })
     }
+
+    ctrl.addTodo = function(){
+        if (!ctrl.newTodo){
+            return;
+        }
+        TodoService.create({
+            title: ctrl.newTodo,
+            completed: false
+        })
+        .then(function(response){
+            ctrl.list.unshift(response);
+            ctrl.newTodo = "";
+        });
+    };
+
+    ctrl.removeTodo = function(item, index){
+        TodoService.remove(item).then(function(response){
+            ctrl.list.splice(index, 1);
+        })
+    };
+
+    ctrl.updateTodo = function (item, index){
+        if(!item.title){
+            ctrl.removeTodo(item, index);
+            return;
+        }
+        TodoService.update(item)
+    };
+
+    ctrl.getRemaining = function(){
+        return ctrl.list.filter(function(item){
+            return !item.completed;
+        });
+    };
+
+    ctrl.toggleState = function(item){
+        TodoService.update(item).then(function(){
+
+        }, function(){
+            item.completed = !item.completed;
+        })
+    }
+
+    getTodos();
 }
 
 angular.module('app').controller('TodoController', TodoController);
